85e9f11659106682e7d5f397348f7bdcd042deda
mladenovic13 <nikolanik999@gmail.com>
2022-08-30 15:29:31 +0200

    deplot api

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,45 +1,52 @@
-import React, { ReactElement, ReactNode } from "react";
-import { fetchPosts, usePosts } from "../hooks/posts";
-import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
-import MainMask from "../components/mask/MainMask";
-import { NextPageWithLayout } from "./_app";
-import BlogPostCardV2 from "../components/cards/BlogPostCardV2";
-import Layout from "../components/layout/Layout";
-import Link from "next/link";
+import React from "react";
 
-const Blog: NextPageWithLayout = () => {
-  const { data } = usePosts();
-
-  return (
-    <div className="pb-24 w-full h-fit py-3 bg-slate-900">
-      <MainMask />
-      <div className="w-full flex-col lg:flex-row flex flex-wrap center gap-5">
-        {data?.map((post) => (
-          <Link key={post.title} href={`/post/${post.slug}`}>
-            <div className="cursor-pointer w-5/6 lg:w-[32%] h-56 ">
-              <BlogPostCardV2 {...post} />
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-// Generating the static props for the blog page
-export const getStaticProps = async () => {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery(["posts"], () => fetchPosts());
-
-  // Return the pages static props
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-};
-Blog.getLayout = (page: ReactElement) => {
-  return <Layout isPost>{page}</Layout>;
+const Blog = () => {
+  return <div>Blog</div>;
 };
 
 export default Blog;
+// import React, { ReactElement, ReactNode } from "react";
+// import { fetchPosts, usePosts } from "../hooks/posts";
+// import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
+// import MainMask from "../components/mask/MainMask";
+// import { NextPageWithLayout } from "./_app";
+// import BlogPostCardV2 from "../components/cards/BlogPostCardV2";
+// import Layout from "../components/layout/Layout";
+// import Link from "next/link";
+
+// const Blog: NextPageWithLayout = () => {
+//   const { data } = usePosts();
+
+//   return (
+//     <div className="pb-24 w-full h-fit py-3 bg-slate-900">
+//       <MainMask />
+//       <div className="w-full flex-col lg:flex-row flex flex-wrap center gap-5">
+//         {data?.map((post) => (
+//           <Link key={post.title} href={`/post/${post.slug}`}>
+//             <div className="cursor-pointer w-5/6 lg:w-[32%] h-56 ">
+//               <BlogPostCardV2 {...post} />
+//             </div>
+//           </Link>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// // Generating the static props for the blog page
+// export const getStaticProps = async () => {
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchQuery(["posts"], () => fetchPosts());
+
+//   // Return the pages static props
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// };
+// Blog.getLayout = (page: ReactElement) => {
+//   return <Layout isPost>{page}</Layout>;
+// };
+
+// export default Blog;
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,61 +1,68 @@
-import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
-import md from "markdown-it";
-import { GetStaticPaths, GetStaticProps } from "next";
-import { ParsedUrlQuery } from "querystring";
-import { ReactElement } from "react";
-import Layout from "../../components/layout/Layout";
-import { fetchPost, fetchPosts, usePost, usePosts } from "../../hooks/posts";
-import { NextPageWithLayout } from "../_app";
+import React from "react";
 
-// The page for each post
-const Post: NextPageWithLayout = ({ slug }: any) => {
-  const { data, isSuccess, isLoading, error } = usePost(slug);
-
-  const render = () => {
-    if (isLoading) return <div>Loading</div>;
-    if (error) return <div>Error</div>;
-    if (isSuccess)
-      return (
-        <main className="prose prose-invert prose-img:rounded-xl ">
-          <div
-            dangerouslySetInnerHTML={{
-              __html: md().render(data.content || ""),
-            }}
-          />
-        </main>
-      );
-  };
-  return <>{render()}</>;
+const Post = () => {
+  return <div>Post</div>;
 };
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = (await fetchPosts()).map((post: Frontmatter) => ({
-    params: { slug: post.slug },
-  }));
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-interface IParams extends ParsedUrlQuery {
-  slug: string;
-}
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const { slug } = context.params as IParams;
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery(["post", slug], () => fetchPost(slug));
-
-  return {
-    props: {
-      slug,
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-};
-
-Post.getLayout = function getLayout(page: ReactElement) {
-  return <Layout isPost>{page}</Layout>;
-};
 export default Post;
+// import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
+// import md from "markdown-it";
+// import { GetStaticPaths, GetStaticProps } from "next";
+// import { ParsedUrlQuery } from "querystring";
+// import { ReactElement } from "react";
+// import Layout from "../../components/layout/Layout";
+// import { fetchPost, fetchPosts, usePost, usePosts } from "../../hooks/posts";
+// import { NextPageWithLayout } from "../_app";
+
+// // The page for each post
+// const Post: NextPageWithLayout = ({ slug }: any) => {
+//   const { data, isSuccess, isLoading, error } = usePost(slug);
+
+//   const render = () => {
+//     if (isLoading) return <div>Loading</div>;
+//     if (error) return <div>Error</div>;
+//     if (isSuccess)
+//       return (
+//         <main className="prose prose-invert prose-img:rounded-xl ">
+//           <div
+//             dangerouslySetInnerHTML={{
+//               __html: md().render(data.content || ""),
+//             }}
+//           />
+//         </main>
+//       );
+//   };
+//   return <>{render()}</>;
+// };
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const paths = (await fetchPosts()).map((post: Frontmatter) => ({
+//     params: { slug: post.slug },
+//   }));
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
+
+// interface IParams extends ParsedUrlQuery {
+//   slug: string;
+// }
+
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const { slug } = context.params as IParams;
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchQuery(["post", slug], () => fetchPost(slug));
+
+//   return {
+//     props: {
+//       slug,
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// };
+
+// Post.getLayout = function getLayout(page: ReactElement) {
+//   return <Layout isPost>{page}</Layout>;
+// };
+// export default Post;
