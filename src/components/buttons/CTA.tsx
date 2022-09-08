ed3db97740350f5e9198f9194d64728f7abb3a23
mladenovic13 <nikolanik999@gmail.com>
2022-09-08 20:16:13 +0200

    cta link

--- src/components/buttons/CTA.tsx
+++ src/components/buttons/CTA.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const CTA = () => {
   return (
-    <button className="font-semibold text-lg mx-auto px-4 py-2 border-2 rounded-3xl animate-pulse">
-      MY PROJECTS
-    </button>
+    <Link href="/projects">
+      <button className="font-semibold text-lg mx-auto px-4 py-2 border-2 rounded-3xl animate-pulse">
+        MY PROJECTS
+      </button>
+    </Link>
   );
 };
 
