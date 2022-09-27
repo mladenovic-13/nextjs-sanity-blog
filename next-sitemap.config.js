31199b507fa063d2890e9110b7cce6f63afa82be
mladenovic13 <nikolanik999@gmail.com>
2022-09-27 14:21:12 +0200

    sitemap url fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://mladenovic13.com",
+  siteUrl: "https://www.mladenovic13.com",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
